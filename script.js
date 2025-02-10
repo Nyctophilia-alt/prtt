4e7f9d1fd28bf0ef8d9340f8fce5418025d27dc2
Semen <h1er0@mail.ru>
2025-02-10 21:36:01 +0300

    изменен тест и справочник

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.addEventListener('click', (e) => {
 function summary() {
   let questions = document.querySelectorAll('.question p'); // Все вопросы
   let score = 0; // Количество верных ответов
-  let rightAnswers = ["a_1", "a_1", "a_1", "a_2", "a_1", "a_1", "a_2", "a_1", "a_2", "a_2"]; // Список верных ответов
+  let rightAnswers = ["a_2", "b_1", "c_1", "d_1", "e_1", "f_1", "g_1", "h_1", "i_1", "j_1", "k_1", "l_1", "m_1", "n_1", "o_1", "p_1", "q_1", "r_1", "s_1", "t_1", "u_1", "v_1", "w_1", "x_1", "y_1", "z_1", "aa_1", "ab_1", "ac_1", "ad_1", "ae_1", "af_1", "ag_1", "ah_1", "ai_1", "aj_1", "ak_1", "al_1", "am_1", "an_1", "ao_1", "ap_1", "aq_1", "ar_1", "as_1", "at_1", "au_1", "av_1", "aw_1", "ax_1"]; // Список верных ответов
 
   questions.forEach((question, index) => {
       let chosenAnswer = document.querySelector(`input[name="вопрос${index + 1}"]:checked`);
